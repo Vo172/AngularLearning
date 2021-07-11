f4a5f1c1c8d8945e83505ff2048617de09d6fcd3
VoPT <anh11221997@gmailcom>
2021-07-11 22:01:02 +0700

    update Day One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DayOneComponent } from './day-one/day-one.component';
+import { DayTwoComponent } from './day-two/day-two.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DayOneComponent,
+    DayTwoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
